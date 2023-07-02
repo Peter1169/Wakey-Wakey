59001ac3218ddf404404276a5636d9a503993535
Peter1169 <46818665+Peter1169@users.noreply.github.com>
2023-07-02 23:09:12 +0200

    Bug fix of the bug fix

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 import os
 import uvicorn
-import asyncio
+import requests
 from fastapi import FastAPI
-import aiohttp
 from threading import Thread
+from time import sleep
 
 # Time (in seconds)
-TIME_INTERVAL = 120 # Interval to make a GET request to an URL
-ERROR_WAIT_TIME = 300 # Interval to make a GET request to an URL after a failed response
+TIME_INTERVAL = 120  # Interval to make a GET request to an URL
+ERROR_WAIT_TIME = 300  # Interval to make a GET request to an URL after a failed response
 
 FILE_NAME = "wake-up.txt"
 SELF_URL = "http://127.0.0.1:8000"
@@ -19,20 +19,19 @@ app = FastAPI()
 async def root():
     return {"message": "WAKE UP SLEEPY HEAD!"}
 
-async def fetch_url(url: str):
+def fetch_url(url: str):
     while True:
         try:
-            async with aiohttp.ClientSession() as session:
-                async with session.get(url) as response:
-                    response.raise_for_status()
-                    json_response = await response.json()
+            response = requests.get(url)
+            response.raise_for_status()
+            json_response = response.json()
             print(f"Response from {url}: {json_response}")
-            await asyncio.sleep(TIME_INTERVAL)
-        except (aiohttp.ClientResponseError, ValueError):
-            print(f"Error or invalid JSON from: {url}")
-            await asyncio.sleep(ERROR_WAIT_TIME)
+            sleep(TIME_INTERVAL)
+        except requests.exceptions.RequestException:
+            print(f"Error from: {url}")
+            sleep(ERROR_WAIT_TIME)
 
-async def url_loop():
+def url_loop():
     if not os.path.isfile(FILE_NAME):
         with open(FILE_NAME, "w") as f:
             f.write(SELF_URL + "\n")
@@ -40,27 +39,15 @@ async def url_loop():
     with open(FILE_NAME, "r") as f:
         urls = f.readlines()
 
-    tasks = [asyncio.create_task(fetch_url(url.strip())) for url in urls]
-    await asyncio.gather(*tasks)
-
-@app.on_event("startup")
-async def startup_event():
-    asyncio.create_task(url_loop())
+    for url in urls:
+        thread = Thread(target=fetch_url, args=(url.strip(),))
+        thread.start()
 
 def run() -> None:
     uvicorn.run(app, host="0.0.0.0", port=8000)
 
-def keep_loop_running(loop):
-    asyncio.set_event_loop(loop)
-    while True:
-        loop.run_forever()
-
 if __name__ == "__main__":
     print("GOODMORNING!!!!")
-    loop = asyncio.new_event_loop()
-    t1 = Thread(target=run)
-    t2 = Thread(target=keep_loop_running, args=(loop,))
-    t1.start()
-    t2.start()
-    t1.join()
-    t2.join()
+    thread = Thread(target=run)
+    thread.start()
+    url_loop()
